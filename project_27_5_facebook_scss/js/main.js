e448a8a96970f8a8c31cbcb31cda8b5b351c9301
tomer <tomer_dahan@hotmail.com>
2018-07-21 22:05:05 +0300

    update from require to import scss

--- project_27_5_facebook_scss/js/main.js
+++ project_27_5_facebook_scss/js/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import {Feed} from './feed';
 import {GetPosts} from "./getposts";
 import {ExtracFromServer} from './services.js';
-require("../scss/style.scss");
+import '../scss/style.scss';
 
 
 new Feed($('.posts'));
